12142d45d119dfc2fe004b1c302b73f8f4466957
tritio <tritio2@gmail.com>
2019-12-10 20:05:40 +0100

    funcionalidad buscar por título

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const axios = require("axios");
+require('dotenv').config();
 
 const URL = process.env.URL_IMDB;
 const API_KEY = process.env.API_KEY;
@@ -9,7 +10,8 @@ router.get("/", async (req, res) => {
     try {
 
         const i = req.query.i;      
-        const urlGet = `${URL}/i=${i}&apikey=${API_KEY}`;
+        const urlGet = `${URL}/?i=${i}&apikey=${API_KEY}`;
+        console.log("la url ", urlGet)
        // http://www.omdbapi.com/?i=tt3896198&apikey=[miApikey]
 
         const response = await axios.get(urlGet);
@@ -18,7 +20,26 @@ router.get("/", async (req, res) => {
         res.status(200).json(response.data);
 
     } catch(error) {
-        console.log(error);
+       // console.log(error);
+        res.status(400).json({error});
+    }
+})
+
+router.get("/titulo", async (req, res) => {
+    try {
+
+        const s = req.query.s;      
+        const urlGet = `${URL}/?s=${s}&apikey=${API_KEY}`;
+        console.log("la url ", urlGet)
+       // http://www.omdbapi.com/?i=tt3896198&apikey=[miApikey]
+
+        const response = await axios.get(urlGet);
+
+        console.log(response.data);
+        res.status(200).json(response.data);
+
+    } catch(error) {
+       // console.log(error);
         res.status(400).json({error});
     }
 })
