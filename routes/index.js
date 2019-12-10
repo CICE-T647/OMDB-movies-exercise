b8f3e352484ab9651535f353990ee62da5bc3f7e
olatzaranzabe <olatzaranzabe@gmail.com>
2019-12-10 12:24:21 +0100

    add pages to search by title

--- routes/index.js
+++ routes/index.js
@@ -9,13 +9,13 @@ module.exports = router;
 router.get("/searchbytitle", async (req, res) => {
     try {
         const { search } = req.query;
+        const page = page > 0 && page <= 100 ? `${page}` : `1`;
         if (search.length < 3) {
             res.status(200).json({ message: "Bad Parameters: Type not found" });
         }
         const response = await axios.get(
-            `http://www.omdbapi.com/?apikey=${myKey}&s=${search}`
+            `${URL}?apikey=${myKey}&s=${search}&page=${page}`
         );
-        res.status(200).json(response.data.Search);
 
         console.log(response);
     } catch (error) {
@@ -33,7 +33,7 @@ router.get("/:type/:title", async (req, res) => {
             res.status(200).json({ message: "Bad Parameters: Type not found" });
         }
         const response = await axios.get(
-            `http://www.omdbapi.com/?apikey=${myKey}&type=${type}&s=${title}`
+            `${URL}?apikey=${myKey}&type=${type}&s=${title}`
         );
         console.log("response", response.data.Search);
 
@@ -53,8 +53,25 @@ router.get("/searchbyyear/:year/:title", async (req, res) => {
             res.status(200).json({ message: "Bad Parameters: Type not found" });
         }
         const response = await axios.get(
-            `http://www.omdbapi.com/?apikey=${myKey}&y=${year}&s=${title}`
+            `${URL}?apikey=${myKey}&y=${year}&s=${title}`
         );
+        console.log("response-length", response.data.Search.length);
+
+        res.status(200).json(response.data.Search);
+    } catch (error) {
+        console.log("error", error);
+        res.status(400).json({ message: "Hay un problema" });
+    }
+});
+
+router.get("/searchbyid/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        console.log("id", id);
+        if (id.length < 3) {
+            res.status(200).json({ message: "Bad Parameters: Type not found" });
+        }
+        const response = await axios.get(`${URL}?apikey=${myKey}&i=${id}`);
         console.log("response", response.data.Search);
 
         res.status(200).json(response.data.Search);
