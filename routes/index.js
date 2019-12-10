c846d5c925d4aceb5836530c21576c1cf4483713
olatzaranzabe <olatzaranzabe@gmail.com>
2019-12-10 11:09:16 +0100

    find by year ok

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,9 @@ module.exports = router;
 router.get("/searchbytitle", async (req, res) => {
     try {
         const { search } = req.query;
-
+        if (search.length < 3) {
+            res.status(200).json({ message: "Bad Parameters: Type not found" });
+        }
         const response = await axios.get(
             `http://www.omdbapi.com/?apikey=${myKey}&s=${search}`
         );
@@ -27,11 +29,31 @@ router.get("/:type/:title", async (req, res) => {
         const { type, title } = req.params;
         console.log("title", title);
         console.log("type", type);
-        if (type === undefined || type.length < 1) {
+        if (type === undefined || type.length < 3 || title.length < 3) {
             res.status(200).json({ message: "Bad Parameters: Type not found" });
         }
         const response = await axios.get(
             `http://www.omdbapi.com/?apikey=${myKey}&type=${type}&s=${title}`
+        );
+        console.log("response", response.data.Search);
+
+        res.status(200).json(response.data.Search);
+    } catch (error) {
+        console.log("error", error);
+        res.status(400).json({ message: "Hay un problema" });
+    }
+});
+
+router.get("/searchbyyear/:year/:title", async (req, res) => {
+    try {
+        const { year, title } = req.params;
+        console.log("year", year);
+        console.log("title", title);
+        if (year === undefined || year.length < 3 || title.length < 3) {
+            res.status(200).json({ message: "Bad Parameters: Type not found" });
+        }
+        const response = await axios.get(
+            `http://www.omdbapi.com/?apikey=${myKey}&y=${year}&s=${title}`
         );
         console.log("response", response.data.Search);
 
